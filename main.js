ade37347682bf9a8e805e165165b0daecd1a777c
Chelsea S <csnider91@live.com>
2023-01-25 00:06:27 -0500

    attempting to create a skeleton framework based off of lecture notes. Content empty currently.

---
+++ main.js
@@ -0,0 +1,14 @@
+(function () {
+   "use strict";
+
+   const source = document.getElementById("etsy-page-template");
+   const template = Handlebars.compile(source);
+
+   const context = {
+    title: "",
+    description: "",
+    price: "",
+   }
+
+  
+})();
